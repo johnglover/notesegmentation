279d8bc818920f524ebe1a8cee7397beebd0acc1
John Glover <j@johnglover.net>
2013-02-15 09:58:23 +0100

    [tests] Add test for frame size == hop size

--- tests/cpp/test_segmentation.cpp
+++ tests/cpp/test_segmentation.cpp
@@ -16,17 +16,17 @@ void TestSegmentation::setUp() {
 void TestSegmentation::test_basic() {
     std::vector<sample> audio(_sf.frames(), 0.0);
     _sf.read(&audio[0], (int)_sf.frames());
-    
+
     int hop_size = _ns.hop_size();
     int frame_size = _ns.frame_size();
     std::vector<int> segments;
 
     for(int i = 0; i <= audio.size() - frame_size; i += hop_size) {
-        segments.push_back(_ns.segment(frame_size, &(audio[i]))); 
+        segments.push_back(_ns.segment(frame_size, &(audio[i])));
     }
 
     CPPUNIT_ASSERT(segments.size() > 0);
-    
+
     for(int i = 0; i < segments.size(); i++) {
         CPPUNIT_ASSERT(segments[i] >= NONE);
         CPPUNIT_ASSERT(segments[i] <= OFFSET);
@@ -36,7 +36,7 @@ void TestSegmentation::test_basic() {
 void TestSegmentation::test_change_hop_frame_sizes() {
     std::vector<sample> audio(_sf.frames(), 0.0);
     _sf.read(&audio[0], (int)_sf.frames());
-    
+
     int frame_size = 512;
     int hop_size = 256;
     std::vector<int> segments;
@@ -45,11 +45,30 @@ void TestSegmentation::test_change_hop_frame_sizes() {
     _ns.hop_size(hop_size);
 
     for(int i = 0; i <= audio.size() - frame_size; i += hop_size) {
-        segments.push_back(_ns.segment(frame_size, &(audio[i]))); 
+        segments.push_back(_ns.segment(frame_size, &(audio[i])));
     }
 
     CPPUNIT_ASSERT(segments.size() > 0);
-    
+
+    for(int i = 0; i < segments.size(); i++) {
+        CPPUNIT_ASSERT(segments[i] >= NONE);
+        CPPUNIT_ASSERT(segments[i] <= OFFSET);
+    }
+
+    frame_size = 256;
+    hop_size = 256;
+    segments.clear();
+
+    _ns.reset();
+    _ns.frame_size(frame_size);
+    _ns.hop_size(hop_size);
+
+    for(int i = 0; i <= audio.size() - frame_size; i += hop_size) {
+        segments.push_back(_ns.segment(frame_size, &(audio[i])));
+    }
+
+    CPPUNIT_ASSERT(segments.size() > 0);
+
     for(int i = 0; i < segments.size(); i++) {
         CPPUNIT_ASSERT(segments[i] >= NONE);
         CPPUNIT_ASSERT(segments[i] <= OFFSET);
@@ -64,11 +83,11 @@ void TestSegmentation::test_change_hop_frame_sizes() {
     _ns.hop_size(hop_size);
 
     for(int i = 0; i <= audio.size() - frame_size; i += hop_size) {
-        segments.push_back(_ns.segment(frame_size, &(audio[i]))); 
+        segments.push_back(_ns.segment(frame_size, &(audio[i])));
     }
 
     CPPUNIT_ASSERT(segments.size() > 0);
-    
+
     for(int i = 0; i < segments.size(); i++) {
         CPPUNIT_ASSERT(segments[i] >= NONE);
         CPPUNIT_ASSERT(segments[i] <= OFFSET);
